7f0bf8733ea657a5db873cdef40842238188221a
Große Perdekamp, Johannes <Johannes.GrossePerdekamp@fmo.de>
2023-05-31 16:49:24 +0200

    added sorting and filtering to posts

--- src/js/utils.js
+++ src/js/utils.js
@@ -14,3 +14,39 @@ export function formatDate(date) {
         timeZone: "UTC",
     })
 }
+
+export function formatBlogPosts(posts, {
+    filterOutDrafts = true,
+    filterOutFuturePosts = true,
+    sortByDate = true,
+    limit = undefined,
+} = {}) {
+    const filteredPosts = posts.reduce((acc, post) => {
+        const {date, draft} = post.frontmatter;
+
+        //filter out drafts if true
+        if(filterOutDrafts && draft) return acc;
+
+        //filter out future posts
+        if(filterOutFuturePosts && new Date(date) > new Date()) return acc;
+
+        //add post to acc
+        acc.push(post)
+
+        return acc;
+    }, [])
+
+    //sort by date or randomize
+    if(sortByDate) {
+        filteredPosts.sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
+    }
+    else {
+        filteredPosts.sort(() => Math.random() - 0.5)
+    }
+
+    //limit number of posts displayed
+    if(typeof limit === "number") {
+        return filteredPosts.slice(0, limit)
+    }
+    return filteredPosts
+}
